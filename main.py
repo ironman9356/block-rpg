ec2a1c7dc1a612654a8f4d7900032eaf15b5022b
ironman9356 <75370527+ironman9356@users.noreply.github.com>
2024-04-02 12:35:07 +0530

    Inital Commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import disnake
+from disnake.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class EconomyBot(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            command_prefix=commands.when_mentioned_or("-"),
+            case_insensitive=True,
+            strip_after_prefix=True
+        )
+
+    async def on_ready(self):
+        print(f"{self.user.name} is now ready to use")
+        print(f"Disnake version: {disnake.__version__}")
+
+
+bot = EconomyBot()
+
+
+token = os.getenv("TOKEN")
+
+bot.run(token)
